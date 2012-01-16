49cb5c3203d75a118cc618d5eebc5e76f9e2ecc3
Xavi <xavi.rmz@gmail.com>
2012-01-15 18:15:42 -0800

    Made sure divide generator doesn't give you 0/0.

--- server.js
+++ server.js
@@ -348,7 +348,7 @@ function grade4() {
         var num3 = Math.floor(10 * Math.random());
         return { question: num1 + " + " + num2 + " + " + num3, answer: num1 + num2 + num3 };
     } else { // Divide
-        var num1 = Math.floor(10 * Math.random()), num2 = Math.floor(10 * Math.random());
+        var num1 = Math.floor(9 * Math.random()) + 1, num2 = Math.floor(9 * Math.random()) + 1;
         var ans = num1 * num2;
         return { question: ans + " / " + num1, answer: num2 };
     }
@@ -374,7 +374,7 @@ function grade5() {
         var num3 = Math.floor(12 * Math.random());
         return { question: num1 + " + " + num2 + " + " + num3, answer: num1 + num2 + num3 };
     } else { // Divide
-        var num1 = Math.floor(12 * Math.random()), num2 = Math.floor(12 * Math.random());
+        var num1 = Math.floor(11 * Math.random()) + 1, num2 = Math.floor(11 * Math.random()) + 1;
         var ans = num1 * num2;
         return { question: ans + " / " + num1, answer: num2 };
     }
